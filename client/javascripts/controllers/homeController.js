ed8ca811f3e50dfdb2794661d2ca201fea7efc68
bahtou <bahtou@users.noreply.github.com>
2016-03-14 18:29:32 -0400

    pre- web component folder structure

---
+++ client/javascripts/controllers/homeController.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('myApp.controllers')
+  .controller('homeController', function() {
+    var controller = this;
+
+    controller.foo = 'bar';
+  });
+
+// function ApplicationController() {
+//   this.foo = 'bar';
+// }
+
+// class ApplicationController {
+//   constructor() {
+//     this.foo = 'bar';
+//   }
+// }
+
+// module.exports = ApplicationController;
